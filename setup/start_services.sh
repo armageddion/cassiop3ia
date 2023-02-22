b7cfe8575ae955ac29f1e3f452127c3a8803c9df
Armageddion <armageddion@gmail.com>
2023-02-22 15:00:12 +0100

    include dotenv

--- setup/start_services.sh
+++ setup/start_services.sh
@@ -1,4 +1,4 @@
-python3 $ALFR3D_HOME_DIR/alfr3d_device/deviceClass.py
-python3 $ALFR3D_HOME_DIR/alfr3d_user/userClass.py
-python3 $ALFR3D_HOME_DIR/alfr3d_environment/environment.py
-python3 $ALFR3D_HOME_DIR/alfr3d_speak/speak.py
+python3 $ALFR3D_HOME_DIR/alfr3d_device/deviceClass.py &
+python3 $ALFR3D_HOME_DIR/alfr3d_user/userClass.py &
+python3 $ALFR3D_HOME_DIR/alfr3d_environment/environment.py &
+python3 $ALFR3D_HOME_DIR/alfr3d_speak/speak.py &
